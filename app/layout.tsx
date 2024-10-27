c06223b8dc0cb852d147236983937c0ae30c4587
Mallikarjuna <51438017+malli7@users.noreply.github.com>
2024-10-27 22:42:25 +0000

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {analytics} from '@vercel/analytics/react'
+import {Analytics} from '@vercel/analytics/react'
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
